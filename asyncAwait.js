bc3644c7d2a679241ed0d3245805472822acc540
RavinakS <ravinasingh697@gmail.com>
2021-08-27 10:49:24 +0530

    error handling with async keyword with catch method

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,18 +1,40 @@
 function a(){
-    return Promise.resolve('a')
+    return new Promise((resolve=>{
+        setTimeout(() => {
+            resolve("A");
+        }, 4000);
+    }))
 }
 
-async function b(){
-    return Promise.resolve('b')
+function b(){
+    return new Promise((resolve=>{
+        setTimeout(() => {
+            resolve('B');
+        }, 4000);
+    }))
 }
 
-async function c(){
-    return 'c'
-}
 
-console.log(a())
-console.log(b())
-console.log(c())
+
+
+
+
+// async function c(){
+//     throw new "Something went wrong!"
+//     // const value = "Ravina!"
+//     // return value;
+// }
+
+// console.log(c());
+// c().then((value)=>{
+//     console.log("Inside then.");
+//     console.log(value);
+// }).catch((err)=>{
+//     console.log("Inside catch!");
+//     console.log(err);
+// })
+
+
 
 // async function hello(){
 //     return "Ravina!"
