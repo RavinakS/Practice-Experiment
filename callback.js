eb98ccdb382893abaaa7b086a275911503c4f3a2
RavinakS <ravinasingh697@gmail.com>
2021-08-27 15:54:34 +0530

    converted the code in callback function

--- callback.js
+++ callback.js
@@ -1,9 +1,13 @@
-const calc = function (num1, num2, calcType){
-    if(calcType === 'add'){
-        return num1 + num2;
-    }else if(calcType === 'multiply'){
-        return num1 * num2;
-    }
+function add(a, b){
+    return a + b;
 };
 
-console.log(calc(2, 3, 'add'));
+function multiply(a, b){
+    return a*b;
+};
+
+const calc = function (num1, num2, callback){
+    return callback(num1, num2);
+};
+
+console.log(calc(2, 3, add));
