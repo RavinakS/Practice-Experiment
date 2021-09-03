570cec3a13a08ec328fd9c831e42a9f36b7fd58b
RavinakS <ravinasingh697@gmail.com>
2021-09-03 15:21:24 +0530

    any dataType that is non-primitive is object type in js

---
+++ nonPrimitive.js
@@ -0,0 +1,10 @@
+const array = ["ravina", 1, 2, "nothing"];
+console.log(typeof array); // object
+
+const object = {
+    1: "one",
+    2: "two"
+};
+console.log(typeof object); // object
+
+// any data type that is not primitive data type, is of Object type in javascript.
