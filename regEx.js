f0ebb6e52061e2612f9c6647d153413eb5c6b0f5
RavinakS <ravinasingh697@gmail.com>
2021-09-11 19:03:49 +0530

    #$

--- regEx.js
+++ regEx.js
@@ -1,3 +1,11 @@
-var r = /3/;
-var s = "123hellp";
-console.log(r.test(s));
+const readline = require('readline-sync');
+
+user = readline.question('>>> ');
+console.log((/[a-zA-Z]/).test(user)); // regular expression - will check if the userinput are from a to z 
+// test is one of the methods of regEx.
+
+// var r = /3/;
+// var s = "123hellp";
+// console.log(r.test(s));
+
+
