9dd442c308ef8e07ce8580a05378d71f61ce68d8
RavinakS <ravinasingh697@gmail.com>
2021-09-11 13:07:07 +0530

    default scope

--- default_scope.js
+++ default_scope.js
@@ -1,7 +1,11 @@
 a = 0;
 while(a<5){
     if(a>2){
-        return a;
+        console.log(a);
+        return a; // it stopes execution when it comes to this line
+        // because Node.js JavaScript is different from browser's javaScript 
+        // when it comes to global scope. In the browser's javascript 
+        // variables declared without var keyword becomes global. In node everythings becomes local by default. 
     }
     a++;
 }
